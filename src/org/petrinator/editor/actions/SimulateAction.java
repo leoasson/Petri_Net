6c9f958308df0aed22ed808a9d00cb8ef0a373d0
leoasson <leoasson@gmail.com>
2017-03-24 12:19:41 -0300

    Initial commit

--- src/org/petrinator/editor/actions/SimulateAction.java
+++ src/org/petrinator/editor/actions/SimulateAction.java
@@ -155,9 +155,10 @@ public class SimulateAction extends AbstractAction {
 				while(true)
 				{
 					try 
-				    {	Thread.currentThread().join();
+				    {	
 				    	m.fireTransition(id);	
-				    } catch (IllegalTransitionFiringError | IllegalArgumentException | PetriNetException | InterruptedException e) {
+				    } catch (IllegalTransitionFiringError | IllegalArgumentException | PetriNetException e) 
+					{
 						e.printStackTrace();
 					}
 				}
